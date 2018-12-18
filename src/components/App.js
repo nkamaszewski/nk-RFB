3b00dd1475078cb8ffc3dbf19e2e9ea938058777
Norbert <norbert.kamaszewski@gmail.com>
2018-12-18 22:12:29 +0100

    end of chapter 8., going to 9

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh seafood market" />
         </div>
         <Inventory />
         <Order />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">Of</span>
-            <span className="the">The</span>
-          </span>
-          day
-        </h1>
-        <h3 className="tagline">
-          <span>Fresh Seafood Market</span>
-        </h3>
-      </header>
-    );
-  }
-}
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
 
 export default Header;
