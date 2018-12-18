739f062785a588b7066b04edc77365df331f8ad6
Norbert <norbert.kamaszewski@gmail.com>
2018-12-18 22:37:40 +0100

    chapter 10. done, going to 11.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
@@ -6,7 +7,7 @@ class StorePicker extends React.Component {
       <Fragment>
         <form className="store-selector">
           <h2>Please enter a Store</h2>
-          <input type="text" required placeholder="Store Name" />
+          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
           <button type="submit">Visit Store -></button>
         </form>
       </Fragment>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './components/App';
+import Router from './components/Router';
 
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
